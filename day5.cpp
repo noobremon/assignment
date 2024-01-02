25ceb04436bfab85f03789a8ec3529961839d18b
Rajarshi Das <rajarshidas729@gmail.com>
2024-01-02 21:10:23 +0530

    first commit

---
+++ day5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the Number1";
+    cin>>num1;
+    cout<<"Enter the Number2";
+    cin>>num2;
+    cout<<"Sum="<<num1+num2;
+
+    return 0;
+}
---
+++ day6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=4,b=5;
+    cout<<"Operators in C++"<<endl;
+    cout<<"following are the types of operators in c++"<<endl;
+    cout<<"The value of a+b :"<<a+b<<endl;
+    cout<<"The value of a-b :"<<a-b<<endl;
+    cout<<"The value of a*b :"<<a*b<<endl;
+    cout<<"The value of a/b :"<<a/b<<endl;
+    cout<<"The value of a+b :"<<a+b<<endl;
+    cout<<"The value of a++ :"<<a++<<endl;
+    cout<<"The value of ++a :"<<++a<<endl;
+    cout<<"The value of a-- :"<<a--<<endl;
+    cout<<"The value of --a :"<<--a<<endl;
+    cout<<endl;
+
+    return 0;
+}
